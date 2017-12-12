9ea4172d9eb4740170d3e75a25e6a0acba606976
Adem ilter <ademilter@gmail.com>
2017-12-13 00:51:06 +0300

    api url change

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,6 +5,6 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   PROJECT_NAMESPACE: '"example"', // localStorage store key name
   NODE_ENV: '"development"',
-  API_URL: '"http://clearsight.protel.com.tr/api"',
+  API_URL: '"http://5a304ec3a871f00012678e6c.mockapi.io/"',
   PORT: '"9000"'
 })
